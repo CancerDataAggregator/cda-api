c2f75e19f2566cf262ac2d02a5e18152cba59f90
tanner-coon-bh <tanner.coon@bluehalo.com>
2025-03-03 13:59:06 -0700

    Reordering routers to be alphabetical order on the Swagger and ReDoc pages

--- cda_api/routers/summary.py
+++ cda_api/routers/summary.py
@@ -10,37 +10,6 @@ from cda_api.models import QNode, SummaryResponseObj
 router = APIRouter(prefix="/summary", tags=["summary"])
 
 
-@router.post("/subject")
-def subject_summary_endpoint(request: Request, qnode: QNode, db: Session = Depends(get_db)) -> SummaryResponseObj:
-    """_summary_
-
-    Args:
-        request (Request): _description_
-        qnode (QNode): _description_
-        db (Session, optional): _description_. Defaults to Depends(get_db).
-
-    Returns:
-        SummaryResponseObj: _description_
-    """
-
-    qid = get_query_id()
-    log = get_logger(qid)
-    log.info(f"summary/subject endpoint hit: {request.client}")
-    log.info(f"QNode: {qnode.as_string()}")
-    log.info(f"{request.url}")
-    if qnode.is_empty():
-        e = EmptyQueryError("Must provide either/both of 'MATCH_ALL' or 'MATCH_SOME' within the request body")
-        log.exception(e)
-        raise HTTPException(status_code=404, detail=str(e))
-
-    try:
-        result = summary_query(db, endpoint_tablename="subject", qnode=qnode, log=log)
-        log.info("Success")
-    except Exception as e:
-        handle_router_errors(e, log)
-    return result
-
-
 @router.post("/file")
 def file_summary_endpoint(request: Request, qnode: QNode, db: Session = Depends(get_db)) -> SummaryResponseObj:
     """_summary_
@@ -66,6 +35,37 @@ def file_summary_endpoint(request: Request, qnode: QNode, db: Session = Depends(
 
     try:
         result = summary_query(db, endpoint_tablename="file", qnode=qnode, log=log)
+        log.info("Success")
+    except Exception as e:
+        handle_router_errors(e, log)
+    return result
+
+
+@router.post("/subject")
+def subject_summary_endpoint(request: Request, qnode: QNode, db: Session = Depends(get_db)) -> SummaryResponseObj:
+    """_summary_
+
+    Args:
+        request (Request): _description_
+        qnode (QNode): _description_
+        db (Session, optional): _description_. Defaults to Depends(get_db).
+
+    Returns:
+        SummaryResponseObj: _description_
+    """
+
+    qid = get_query_id()
+    log = get_logger(qid)
+    log.info(f"summary/subject endpoint hit: {request.client}")
+    log.info(f"QNode: {qnode.as_string()}")
+    log.info(f"{request.url}")
+    if qnode.is_empty():
+        e = EmptyQueryError("Must provide either/both of 'MATCH_ALL' or 'MATCH_SOME' within the request body")
+        log.exception(e)
+        raise HTTPException(status_code=404, detail=str(e))
+
+    try:
+        result = summary_query(db, endpoint_tablename="subject", qnode=qnode, log=log)
         log.info("Success")
     except Exception as e:
         handle_router_errors(e, log)
