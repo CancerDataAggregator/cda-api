dc49fd92a9288cdc67e39c2f9329ad85f6cad43b
tanner-coon-bh <tanner.coon@bluehalo.com>
2026-06-11 10:49:35 -0600

    Fixing bug that enforces summary return columns. Also adding new unit tests

--- tests/test_data_request_body_functionality.py
+++ tests/test_data_request_body_functionality.py
@@ -418,21 +418,194 @@ def test_collate_results_add_multiple_tables():
 
 
 
+################################ EXTERNAL_REFERENCE ################################
+def test_external_reference_popultated_result():
+    response = client.post(
+        "/data/subject",
+        json={
+              "MATCH_ALL": ["subject_id like APOLLO.AP-2*"], # both subject's with id like "APOLLO.AP-2*" appear to have external_references
+              "EXTERNAL_REFERENCE": True
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    # The external_reference_columns should contain a list of dictonaries for the rows of related external references
+    assert 'external_reference_columns' in response.json()['result'][0].keys()
+    assert isinstance(response.json()['result'][0]['external_reference_columns'], list)
+    assert isinstance(response.json()['result'][0]['external_reference_columns'][0], dict)
+    assert 'external_reference_type' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'external_reference_name' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'external_reference_short_name' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'last_updated' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'uri' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'external_reference_description' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'source_short_name' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'source_url' in response.json()['result'][0]['external_reference_columns'][0].keys()
+
+def test_external_reference_null_result():
+    response = client.post(
+        "/data/subject",
+        json={
+              "MATCH_ALL": ["subject_id like ALCHEMIST.ALCH-ABB*"], # both subject's with id like "ALCHEMIST.ALCH-ABB*" appear to not have external_references
+              "EXTERNAL_REFERENCE": True
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    # The external_reference_columns should be empty if there are no related external references
+    assert 'external_reference_columns' in response.json()['result'][0].keys()
+    assert response.json()['result'][0]['external_reference_columns'] == None
+
+
+################################ SEARCH_LIST ################################
+def test_search_list_single_keyword_match():
+    response = client.post(
+        "/data/subject",
+        json={
+              "SEARCH_LIST": ['lung'] # 'lung' will match a keyword so a text_search ts_query will be not initiated
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    assert 'unified_keyword_preselect' in response.json()['query_sql']
+    assert 'unmatched_keyword_text_search_preselect' not in response.json()['query_sql']
+    assert len(response.json()['result']) >= 1
+
+def test_search_list_data_file_endpoint():
+    response = client.post(
+        "/data/file",
+        json={
+              "SEARCH_LIST": ['lung'] # 'lung' will match a keyword so a text_search ts_query will be not initiated
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    assert 'unified_keyword_preselect' in response.json()['query_sql']
+    assert 'unmatched_keyword_text_search_preselect' not in response.json()['query_sql']
+    assert len(response.json()['result']) >= 1
+
+def test_search_list_two_keyword_matches():
+    response = client.post(
+        "/data/subject",
+        json={
+              "SEARCH_LIST": ['lung', 'male'] 
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    assert 'unified_keyword_preselect' in response.json()['query_sql']
+    assert 'unmatched_keyword_text_search_preselect' not in response.json()['query_sql']
+    assert len(response.json()['result']) >= 1
+
+def test_search_list_single_ts_vector_match():
+    response = client.post(
+        "/data/subject",
+        json={
+              "SEARCH_LIST": ['lungs'] # 'lungs' won't match a keyword so a text_search ts_query will be initiated
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    assert 'unified_keyword_preselect' not in response.json()['query_sql']
+    assert 'unmatched_keyword_text_search_preselect' in response.json()['query_sql']
+    assert len(response.json()['result']) >= 1
+
+def test_search_list_two_ts_vector_matches():
+    response = client.post(
+        "/data/subject",
+        json={
+              "SEARCH_LIST": ['lungs', 'males']
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    assert 'unified_keyword_preselect' not in response.json()['query_sql']
+    assert 'unmatched_keyword_text_search_preselect' in response.json()['query_sql']
+    assert len(response.json()['result']) >= 1
+
+def test_search_list_wildcard_keyword_match():
+    response = client.post(
+        "/data/subject",
+        json={
+              "SEARCH_LIST": ['male*'] # 'male*' matches multiple keywords and therefore should not create a ts_vector
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    assert 'unified_keyword_preselect' in response.json()['query_sql']
+    assert 'unmatched_keyword_text_search_preselect' not in response.json()['query_sql']
+    assert len(response.json()['result']) >= 1
+
+def test_search_list_no_matching_wildcard_keyword_error():
+    response = client.post(
+        "/data/subject",
+        json={
+              "SEARCH_LIST": ['lungs*'] # 'lungs*' won't match any keywords and therefore will throw an error
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 400
+    assert response.json()['error_type'] == 'InvalidSearchError'
+    assert response.json()['message'] == 'Term with wildcard: \"lungs*\" yielded no results'
+
+
+def test_search_list_keyword_and_ts_vector_matches():
+    response = client.post(
+        "/data/subject",
+        json={
+              "SEARCH_LIST": ['lung', 'males'] # 'lung' match a keyword but 'males' won't so a both the keyword preselect and the text_search ts_query will be initiated
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    assert 'unified_keyword_preselect' in response.json()['query_sql']
+    assert 'unmatched_keyword_text_search_preselect' in response.json()['query_sql']
+    assert len(response.json()['result']) >= 1
+
+def test_search_list_file_only_match():
+    response = client.post(
+        "/data/subject",
+        json={
+              "SEARCH_LIST": ['cortex of kidney'] # 'cortex of kidney' will match in file keywords but not in subject keywords only the file keyword preselect will be created 
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    assert 'unified_keyword_preselect' not in response.json()['query_sql']
+    assert 'unmatched_keyword_text_search_preselect' not in response.json()['query_sql']
+    assert 'subject_exclusive_keywords_preselect' not in response.json()['query_sql']
+    assert 'file_exclusive_keywords_preselect' in response.json()['query_sql']
+
+def test_search_list_no_matching_keyword_or_ts_query():
+    response = client.post(
+        "/data/subject",
+        json={
+              "SEARCH_LIST": ['BADTERM'] # This term should yield an empty result
+              },
+        params={'limit':10}
+    )
+    assert response.status_code == 200
+    assert len(response.json()['result']) == 0
+
+
+
 ################################ All Together Now ################################
 def test_request_body_data_subject():
     response = client.post(
         "/data/subject",
         json={
-              "MATCH_ALL": ["subject_id_alias < 500", "species is not null"],
+              "SEARCH_LIST": ["lung*", "males"],
+              "MATCH_ALL": ["subject_id like APOLLO.AP-2*", "species is not null"],
               "MATCH_SOME": ["diagnosis like a*", "sex like f*"],
               "ADD_COLUMNS": ["observation.*", "file.*"],
               "EXCLUDE_COLUMNS": ["ethnicity", "diagnosis"],
-              "COLLATE_RESULTS": True
+              "COLLATE_RESULTS": True,
+              "EXTERNAL_REFERENCE": True
               },
         params={'limit':10}
     )
     assert response.status_code == 200
-    assert len(response.json()['result'][0]) > 1
+    assert len(response.json()['result'][0]) >= 1
     assert "ethnicity" not in response.json()['result'][0].keys()
     assert 'observation_columns' in response.json()['result'][0].keys()
     assert isinstance(response.json()['result'][0]['observation_columns'], list)
@@ -444,3 +617,16 @@ def test_request_body_data_subject():
     assert isinstance(response.json()['result'][0]['file_columns'][0], dict)
     assert 'file_type' in response.json()['result'][0]['file_columns'][0].keys()
     assert 'size' in response.json()['result'][0]['file_columns'][0].keys()
+    assert 'unified_keyword_preselect' in response.json()['query_sql']
+    assert 'unmatched_keyword_text_search_preselect' in response.json()['query_sql']
+    assert 'external_reference_columns' in response.json()['result'][0].keys()
+    assert isinstance(response.json()['result'][0]['external_reference_columns'], list)
+    assert isinstance(response.json()['result'][0]['external_reference_columns'][0], dict)
+    assert 'external_reference_type' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'external_reference_name' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'external_reference_short_name' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'last_updated' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'uri' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'external_reference_description' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'source_short_name' in response.json()['result'][0]['external_reference_columns'][0].keys()
+    assert 'source_url' in response.json()['result'][0]['external_reference_columns'][0].keys()
